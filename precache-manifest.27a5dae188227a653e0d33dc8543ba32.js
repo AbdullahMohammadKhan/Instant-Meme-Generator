e812e73fa0a850aeec16d7f5c69fe464f36f8dd8
abdullah <hbkamk@gmail.com>
2020-07-17 20:10:47 +0600

    Updates

---
+++ precache-manifest.27a5dae188227a653e0d33dc8543ba32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f99c420545c6f7aa7c916fb4a9b14d4e",
+    "url": "/Instant-Meme-Generator/index.html"
+  },
+  {
+    "revision": "6874f842f8ca7e29fb59",
+    "url": "/Instant-Meme-Generator/static/css/main.9a4f4f76.chunk.css"
+  },
+  {
+    "revision": "f8d150f6041c12ae64fc",
+    "url": "/Instant-Meme-Generator/static/js/2.c3099321.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Instant-Meme-Generator/static/js/2.c3099321.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6874f842f8ca7e29fb59",
+    "url": "/Instant-Meme-Generator/static/js/main.1d8aa193.chunk.js"
+  },
+  {
+    "revision": "b3314fd9f223bc1cb06a",
+    "url": "/Instant-Meme-Generator/static/js/runtime-main.f2e52783.js"
+  }
+]);
